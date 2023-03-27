9b8ec1eb49afdd5509ed9eaa1f6b602ae6642302
Canody <96164945+Canody@users.noreply.github.com>
2023-03-27 17:13:18 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PkuTomo'
+copyright = '2023, Yao Zhang'
+author = 'Yao Zhang'
 
 release = '0.1'
 version = '0.1.0'
